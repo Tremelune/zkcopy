12c30ff58c95f61150d4ae83c50a6f83f4845c43
Will benedict <wbenedict@squarespace.com>
2016-10-26 16:08:15 -0400

    Moving clients around...

--- src/main/java/com/github/ksprojects/zkcopy/sync/Transferer.java
+++ src/main/java/com/github/ksprojects/zkcopy/sync/Transferer.java
@@ -3,19 +3,24 @@ package com.github.ksprojects.zkcopy.sync;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.github.ksprojects.zkcopy.Node;
 import com.github.ksprojects.zkcopy.writer.Writer;
 
-// todo What decides the server to use for this shit? Config? Switch?
 public class Transferer {
   private static final Logger LOG = LoggerFactory.getLogger(Transferer.class);
-  private static final String LOCAL_SERVER = "localhost";
+
+  private final CuratorFramework localClient;
+  private final CuratorFramework remoteClient;
+
+
+  Transferer(CuratorFramework localClient, CuratorFramework remoteClient) {
+    this.localClient = localClient;
+    this.remoteClient = remoteClient;
+  }
 
 
   void copy(String fromServer, String toServer, ZooKeeperPath path) {
@@ -29,24 +34,20 @@ public class Transferer {
     Writer writer = new Writer(target, node, false);
     writer.write();
 
-    verify(target, node, path);
+    verify(remoteClient, node, path);
   }
 
 
   public Node readLocal(ZooKeeperPath path) {
-    return read(path);
+    return read(localClient, path);
   }
 
   //  Node readRemote(ZooKeeperPath path) {
   //    return read(REMOTE_SERVER, path);
   //  }
 
-  private Node read(ZooKeeperPath path) {
+  private Node read(CuratorFramework client, ZooKeeperPath path) {
     try {
-      RetryPolicy retryPolicy = (i, l, retrySleeper) -> false;
-      CuratorFramework curator = CuratorFrameworkFactory.newClient("localhost:2181", retryPolicy);
-      curator.start();
-      CuratorFramework client = curator.usingNamespace("warden");
       Node root = new Node(path.path);
       read(client, root);
       return root;
@@ -56,11 +57,11 @@ public class Transferer {
   }
 
 
-  private void verify(String toServer, Node fromNode, ZooKeeperPath path) {
-    Node updatedNode = read(path);
+  private void verify(CuratorFramework client, Node fromNode, ZooKeeperPath path) {
+    Node updatedNode = read(client, path);
 
     if (!Arrays.equals(updatedNode.getData(), fromNode.getData())) {
-      throw new SyncException("New value not written to: " + toServer);
+      throw new SyncException("New value not written to: " + path);
     }
   }
 
--- src/test/java/com/github/ksprojects/zkcopy/sync/TransfererIT.java
+++ src/test/java/com/github/ksprojects/zkcopy/sync/TransfererIT.java
@@ -1,6 +1,7 @@
 package com.github.ksprojects.zkcopy.sync;
 
 import static com.github.ksprojects.zkcopy.sync.ZooKeeperPath.SETTINGS;
+import static com.github.kxprojects.zkcopy.TestUtils.localClient;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.fail;
 
@@ -10,17 +11,16 @@ import org.apache.curator.framework.CuratorFramework;
 import org.testng.annotations.Test;
 
 import com.github.ksprojects.zkcopy.Node;
-import com.github.kxprojects.zkcopy.TestUtils;
 
 public class TransfererIT {
   @Test
   public void testRead() throws Exception {
     int random = new Random().nextInt();
-    CuratorFramework curator = TestUtils.localClient();
+    CuratorFramework curator = localClient();
     byte[] payload = String.valueOf(random).getBytes();
     curator.setData().forPath("/settings/test", payload);
 
-    Transferer underTest = new Transferer();
+    Transferer underTest = new Transferer(localClient(), null);
     Node node = underTest.readLocal(SETTINGS);
     Node readSetting = null;
     for (Node child : node.getChildren()) {
